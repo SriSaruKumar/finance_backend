3113ab82896357e9f335161cb8360f7c049ad23f
SriSaruKumar <145571840+SriSaruKumar@users.noreply.github.com>
2025-08-23 02:02:59 +0530

    Update firebase.py

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -5,7 +5,8 @@ import firebase_admin
 from firebase_admin import credentials
 
 # Decode the environment variable
-service_account_info = json.loads(base64.b64decode(os.environ["FIREBASE_SERVICE_ACCOUNT"]))
+decoded_json = base64.b64decode(os.environ["FIREBASE_SERVICE_ACCOUNT"]).decode("utf-8")
+service_account_info = json.loads(decoded_json)
 
 cred = credentials.Certificate(service_account_info)
 firebase_admin.initialize_app(cred)
